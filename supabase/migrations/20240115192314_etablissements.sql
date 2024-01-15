5e399c7cb68ffa88948870f3b450205e92df44a9
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-01-15 21:51:46 +0100

    Initial commit

---
+++ supabase/migrations/20240115192314_etablissements.sql
@@ -0,0 +1,17 @@
+create extension postgis schema extensions;
+
+create table etablissements (
+  etablissement_id bigint not null,
+  nom text,
+  voie text,
+  codepostal text,
+  ville text,
+  coordonnees extensions.geometry(Point, 4326) default null::geometry
+);
+
+alter table etablissements
+  add constraint cinemas_pkey primary key (etablissement_id);
+
+create index cinema_coordonnees_idx
+  on etablissements
+  using GIST (coordonnees);
