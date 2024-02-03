e997ee97ff793fd3c513d34b2d780f410540764d
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-02-03 10:53:12 +0100

    view acteur

---
+++ supabase/migrations/20240203094918_views.sql
@@ -0,0 +1,23 @@
+
+create materialized view acteur as
+  select p.personne_id,
+  case
+    when (p.artiste is not null) then (p.artiste)::text
+    else (((p.prenom)::text || ' '::text) || (p.nom)::text)
+  end as nom,
+  p.naissance,
+  case
+    when (p.deces is null) then date_part('year'::text, age((p.naissance)::timestamp with time zone))
+    else null::double precision
+  end as age,
+  p.deces,
+  p.nationalite,
+  p.photo,
+  count(c.*) as nb_film
+  from (equipes c
+    join personnes p on (c.personne_id = p.personne_id))
+  where ((c.role)::text = 'acteur'::text)
+  group by p.personne_id
+with no data;
+
+refresh materialized view acteur with data;
