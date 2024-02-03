ba86750ded5f54c026d4ab6eb442497ed48f93d7
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-02-03 10:02:59 +0100

    equipes

---
+++ supabase/migrations/20240203085840_equipes.sql
@@ -0,0 +1,19 @@
+create table if not exists equipes (
+  film_id uuid not null,
+  personne_id uuid not null,
+  role text,
+  alias text,
+  constraint equipe_film_fk foreign key (film_id) references films(film_id)
+    on update no action on delete no action not valid,
+  constraint equipe_personne_fk foreign key (personne_id) references personnes(personne_id)
+    on update no action on delete no action not valid
+);
+
+create index equipe_films_fki
+  on equipes(film_id);
+
+create index equipe_personnes_fki
+  on equipes(personne_id);
+
+comment on table equipes is
+  e'@foreignkey (personne) references cinema.acteur(id)|@fieldname rolebyacteur';
