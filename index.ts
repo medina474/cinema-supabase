5b426b636b5efab6e0d60f48f439b59e64269cdf
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-02-10 00:13:49 +0100

    mass import

--- index.ts
+++ index.ts
@@ -1,48 +1,64 @@
 import sql from './db.js'
 
-async function insertFilm(personne_id, credit) {
+async function insertFilm(identifiant:string, uuid, credit) {
+
   const result = await sql`
     select id from links
-    where identifiant = ${credit.id}
-  `
+    where identifiant = ${credit.id}`;
+
   if (result.count == 0) {
-    const film = await sql`
-    insert into films (titre, titre_original)
-    values (${credit.title}, ${credit.original_title}) returning films.film_id`
+    try {
+      const film = await sql`insert into films (titre, titre_original, sortie, vote_votants)
+      values (${credit.title}, ${credit.original_title}, ${credit.release_date}, ${credit.vote_count})
+      returning films.film_id`
 
-    await sql`
-    insert into equipes (film_id, personne_id, role, alias)
-    values (${film[0].film_id}, ${personne_id},  'acteur', ${credit.character})`
+      await sql`insert into equipes (film_id, personne_id, role, alias)
+      values (${film[0].film_id}, ${uuid}, 'acteur', ${credit.character})`
 
-    await sql`
-    insert into resumes (film_id, langue_code, resume)
-    values (${film[0].film_id}, 'fra',  ${credit.overview})`
+      await sql`insert into resumes (film_id, langue_code, resume)
+      values (${film[0].film_id}, 'fra', ${credit.overview})`
 
-    await sql`
-    insert into links (id, site, identifiant)
-    values (${film[0].film_id}, 1,  ${credit.id})`;
+      await sql`insert into links (id, site, identifiant)
+      values (${film[0].film_id}, 1,  ${credit.id})`;
 
-    for (let g of credit.genre_ids) {
-      await sql`
-      insert into films_genres (film_id, genre_id)
-      values (${film[0].film_id}, ${g})`
+      for (let g of credit.genre_ids) {
+        await sql`insert into films_genres (film_id, genre_id)
+        values (${film[0].film_id}, ${g})`
+      }
+    } catch (e) {
+      console.log(JSON.stringify(credit));
     }
+  }
+  else {
+
   }
 }
 
-const tmdb = 1100;
 
-const data = await fetch(`https://api.themoviedb.org/3/person/${tmdb}/combined_credits?language=fr-FR`, {
-  method: 'get',
-  headers: new Headers({
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmE0Y2YxZDUwNzlkOTMwYzA3YmVjYmJhZTBjNDI4YyIsInN1YiI6IjYwM2U5ZjE3ODQ0NDhlMDAzMDBlZWQwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9CBeYye4C17jp29j77VjChML6ZJLwObLSolQW2GAhU4',
-    'accept': 'application/json'
-  })
-});
+const personne = await sql`
+    SELECT l.identifiant, p.nom,
+    p.prenom,
+    p.personne_id
+   FROM personnes p
+     JOIN links l ON p.personne_id = l.id AND l.site = 1`
+for (let p of personne)
+{
 
-const json = await data.json();
+  const data = await fetch(`https://api.themoviedb.org/3/person/${p.identifiant}/combined_credits?language=fr-FR`, {
+    method: 'get',
+    headers: new Headers({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmE0Y2YxZDUwNzlkOTMwYzA3YmVjYmJhZTBjNDI4YyIsInN1YiI6IjYwM2U5ZjE3ODQ0NDhlMDAzMDBlZWQwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9CBeYye4C17jp29j77VjChML6ZJLwObLSolQW2GAhU4',
+      'accept': 'application/json'
+    })
+  });
 
-for (const c of json.cast.filter(e => e.media_type == 'movie' && e.popularity > 10)) {
-console.log(`${c.id} ${c.title}`);
-  insertFilm('450c6276-60bc-4791-bee4-3152145af6e7', c);
-}
+  const json = await data.json();
+
+  json.cast.filter(e => e.media_type == 'movie' && e.popularity > 10 && e.release_date < '1991-01-01' && e.order < 10)
+    .sort((a, b) => b.popularity - a.popularity)
+    .slice(0, 5)
+    .forEach(async element => {
+      console.log(`${p.nom} ${element.title}`);
+      await insertFilm(p.identifiant, p.personne_id, element);
+    });
+  }
