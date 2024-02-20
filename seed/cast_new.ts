1d423d0d854968c8f30191889a80381356d843a1
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-02-20 22:01:22 +0100

    ordre equipe

--- seed/cast_new.ts
+++ seed/cast_new.ts
@@ -24,30 +24,46 @@ for (const film of films) {
     continue;
   }
 
-  json.credits.cast.forEach(async credit => {
+  json.credits.cast.filter(elt => elt.popularity > 30)
+    .sort((a, b) => b.order - a.order)
+    .slice(1, 5)
+    .forEach(async credit => {
 
       const personnes = await sql`select * from links l
         inner join personnes p on p.personne_id = l.id
         where identifiant = ${credit.id} and site_id = 1`;
 
-      if (personnes.count == 1) {
-        const personne_id = personnes[0].id
+      if (personnes.count == 0) {
+        console.log(`${credit.name} : ${credit.character} (${credit.order}) ${credit.popularity}`);
 
+        if (credit.order < 6) {
+          const parts = credit.name.split(' ');
+          const personne = await sql`insert into personnes (nom, prenom)
+            values (${parts[1]}, ${parts[0]})
+            returning personnes.personne_id`;
+
+          await sql`insert into links (id, site_id, identifiant)
+            values (${personne[0].personne_id}, 1,  ${credit.id})`;
+
+          await sql`insert into equipes (film_id, personne_id, role, alias, ordre)
+            values (${film.film_id}, ${personne[0].personne_id}, 'acteur', ${credit.character}, ${credit.order})`
+        }
+
+      }
+      else
+      {
+        const personne_id = personnes[0].id
         const equipe = await sql`select alias from equipes
           where film_id = ${film.film_id} and personne_id = ${personne_id} and role = 'acteur'`;
 
-        if (equipe.count == 0) {
+          if (equipe.count == 0) {
           try {
-            console.log(`  + ${credit.name} as ${credit.character}`);
+            console.log(`${film.titre} -> ${credit.name} as ${credit.character}`);
             await sql`insert into equipes (film_id, personne_id, role, alias, ordre)
                 values (${film.film_id}, ${personne_id}, 'acteur', ${credit.character}, ${credit.order})`
           } catch (error) {
             console.log(error)
           }
-        }
-        else {
-          await sql`update equipes set ordre = ${credit.order}
-            where film_id = ${film.film_id} and personne_id = ${personne_id} and role = 'acteur'`
         }
       }
     });
