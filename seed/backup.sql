e1b4c0b98a9612177225ff1786179c81b8f7a29e
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-02-21 22:51:36 +0100

    productions

--- seed/backup.sql
+++ seed/backup.sql
@@ -3,3 +3,6 @@
 \copy links_societes to '../data/links_societes.csv' (format csv, header, encoding 'utf8');
 \copy links_personnes to '../data/links_personnes.csv' (format csv, header, encoding 'utf8');
 \copy links_films to '../data/links_films.csv' (format csv, header, encoding 'utf8');
+
+\copy societes to '../data/societes.csv' (format csv, header, encoding 'utf8');
+\copy productions to '../data/productions.csv' (format csv, header, encoding 'utf8');
