6e593050d2222eb98617c0771a02bdef1ff9cfa3
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2024-03-01 21:35:04 +0100

    maj

--- supabase/migrations/20240115214448_genres.sql
+++ supabase/migrations/20240115214448_genres.sql
@@ -2,3 +2,8 @@ create table genres (
   genre_id integer not null primary key,
   genre text not null
 );
+
+create table motscles (
+  motcle_id integer not null primary key,
+  motcle text not null
+);
